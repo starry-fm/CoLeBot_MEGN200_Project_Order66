d92dd3ec420a276aeb4e9af5b830cce4553b0ca0
BenjiLimeRidge <benji_salmon@mines.edu>
2025-11-21 14:55:36 -0700

    Refactor transmitter code and add OLED display
    Refactor transmitter code to improve structure and readability. Added OLED display functionality, button handling, and DHT sensor integration.

--- transmitter.ino
+++ transmitter.ino
@@ -1,106 +1,90 @@
 #include "WifiPort2.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "DHT.h"
 
-int Button1Pin = 2; //assign buttons to digital pins
-int Button2Pin = 3;
-int Button3Pin = 4;
-
-int LJoyStickYPin = A0; //assign joysticks to analog pins
-int LcenteredJoystickY;
-
-int RJoyStickYPin = A1;
-int RcenteredJoystickY;
-
-int RJoyStickButton = 5;
-
-// A structure, similar to our servo and stepper motors, but this one conatins variables to be transmitted
-// Any variable you want to transmit/recieve must be initalized in the DataPacket structure
+// ----- WiFi / DataPacket -----
 struct DataPacket {
-
-  int AnalogCheck;  //an initial check to show successful transmission
-                    //YOU should wire up a simple resistor (220 ohm) circuit and manually probe it with a wire connected to A0
-  int LJoyStickYValue; //create variables for data packet which communciate input values to the receiver
-  int RJoyStickYValue;
-
-  int Button1Pressed;
-  int Button2Pressed;
-  int Button3Pressed;
-  int RJoyButtonPressed;
-
+  int AnalogCheck;     // debug / timestamp
+  bool ButtonPressed;  // button state
 } data;
-
-//gloabl vars are outside datapacket
 
 WifiPort<DataPacket> WifiSerial;
 
-void setup() {
-  //DONT USE PIN13 FOR ANY SENSOR OR ACTUATORS
+// ----- Pins -----
+const int buttonPin = 3;
+const int DHTPin = 2;
+#define DHTTYPE DHT11
 
-  Serial.begin(115200);  //preferred transmission rate for Arduino UNO R4
-  pinMode(Button1Pin, INPUT_PULLUP);
-  pinMode(Button2Pin, INPUT_PULLUP);
-  pinMode(Button3Pin, INPUT_PULLUP);
-  pinMode(RJoyStickButton, INPUT_PULLUP);
-  pinMode(LJoyStickYPin, INPUT);
-  pinMode(RJoyStickYPin, INPUT);
-  // WifiSerial.begin("ssid_UPDATE_FOR_YOUR_GROUP", "password_UPDATE", WifiPortType::Receiver);
-  WifiSerial.begin("ssid_PairAP_CBF66", "passwordAP_07654321", WifiPortType::Transmitter);
-  //WifiSerial.begin("ssid_PairAP_CBF66", "passwordAP_07654321", WifiPortType::Emulator); // one board to rule them all debugging
+// ----- DHT Sensor -----
+DHT dht(DHTPin, DHTTYPE);
+
+// ----- OLED Display -----
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// ----- Variables -----
+bool lastButtonState = false;
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize button
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Initialize DHT
+  dht.begin();
+
+  // Initialize OLED
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (1);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+
+  // Initialize WiFi as transmitter
+  WifiSerial.begin("group170dropper", "dropperYeah", WifiPortType::Transmitter);
+  Serial.println("WiFi transmitter ready");
 }
 
 void loop() {
+  // Update debug variable
+  data.AnalogCheck = millis();
 
-  if (WifiSerial.getPortType() == WifiPortType::Transmitter || WifiSerial.getPortType() == WifiPortType::Emulator) {
-    WifiSerial.autoReconnect();//try and connect
-
-    //Tx stuff below
-    data.AnalogCheck = millis(); // analogRead(A0);
-    Serial.print("Sending: " );
-    Serial.println(data.AnalogCheck);
-    //Tx stuff above
-
-
-    if (!WifiSerial.sendData(data))//check and see if connection is established and data is sent
-      Serial.println("Wifi Send Problem");//oh no it didn't send --> it iwll try and re-connect at the start of the loop
-
+  // ----- Button handling -----
+  bool buttonState = !digitalRead(buttonPin); // pressed = HIGH
+  if (buttonState != lastButtonState) {
+    data.ButtonPressed = buttonState;
+    lastButtonState = buttonState;
+    Serial.print("ButtonPressed: "); Serial.println(data.ButtonPressed);
   }
 
-
-  
-  if ((WifiSerial.getPortType() == WifiPortType::Receiver || WifiSerial.getPortType() == WifiPortType::Emulator) && WifiSerial.checkForData()) {
-
-    data = WifiSerial.getData();//received and unpack data structure
-
-    //all Rx stuff below
-    Serial.print("Received: " );
-    Serial.println(data.AnalogCheck); 
-    //all RX stuff above
-
+  // ----- Send data over WifiPort2 -----
+  if (!WifiSerial.sendData(data)) {
+    Serial.println("WiFi Send Problem");
   }
 
- // Read buttons
-data.Button1Pressed = (digitalRead(Button1Pin) == LOW); //update data packet to tell receiver if buttons are pressed or not. If button is pressed, variable updated to true
-data.Button2Pressed = (digitalRead(Button2Pin) == LOW);
-data.Button3Pressed = (digitalRead(Button3Pin) == LOW);
-data.RJoyButtonPressed = (digitalRead(RJoyStickButton)==LOW);
+  // ----- Read DHT11 -----
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
 
-// Read joysticks
-data.LJoyStickYValue = analogRead(LJoyStickYPin) - 512; //update data packet to tell receiver what the values of the joysticks are, setting the center to zero and extremes to +/- 512
-data.RJoyStickYValue = analogRead(RJoyStickYPin) - 512;
+  // ----- Update OLED -----
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Temp: ");
+  if (isnan(temp)) display.print("Err"); else display.print(temp);
+  display.println(" C");
 
+  display.print("Humidity: ");
+  if (isnan(hum)) display.print("Err"); else display.print(hum);
+  display.println(" %");
 
-if (data.Button1Pressed || data.Button2Pressed || data.Button3Pressed || data.RJoyButtonPressed) { //button debugging
-  Serial.print("Buttons: B1=");
-  Serial.print(data.Button1Pressed);
-  Serial.print(" B2=");
-  Serial.print(data.Button2Pressed);
-  Serial.print(" B3=");
-  Serial.println(data.Button3Pressed);
-  Serial.print(" JB=");
-  Serial.println(data.RJoyButtonPressed);
-}
+  display.display();
 
-
-  
-
-  delay(10);
+  delay(200); // small delay for sensor and OLED
 }
